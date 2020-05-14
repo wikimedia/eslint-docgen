7e16bd95ba4170745a632afef076474bce4f7e5e
Ed Sanders <ejsanders@gmail.com>
2020-05-14 16:45:50 +0100

    Separate out fixing code

--- src/build-docs-from-tests.js
+++ src/build-docs-from-tests.js
@@ -1,26 +1,13 @@
 const ejs = require( 'ejs' );
 const path = require( 'path' );
-const eslint = require( 'eslint' );
-const linter = new eslint.Linter();
 const fs = require( 'fs' );
 const rulePath = 'src/rules/{name}.js';
 const docPath = 'docs/{name}.md';
 
 const rulesData = require( './rules-data' );
-const cli = new eslint.CLIEngine();
-const config = cli.getConfigForFile( require( './main' ) );
 
 const packagePath = require( './package-path' );
 const pluginName = require( packagePath( './package' ) ).name.replace( 'eslint-plugin-', '' );
-
-// Restrict config used by verifyAndFix to fixable formatting rules
-// TODO: Do this by looking at rule metadata
-const formattingRuleNames = [ 'array-bracket-spacing', 'block-spacing', 'brace-style', 'comma-dangle', 'comma-spacing', 'comma-style', 'computed-property-spacing', 'curly', 'dot-location', 'dot-notation', 'func-call-spacing', 'indent', 'key-spacing', 'keyword-spacing', 'linebreak-style', 'no-extra-semi', 'no-irregular-whitespace', 'no-mixed-spaces-and-tabs', 'no-multi-spaces', 'no-regex-spaces', 'no-tabs', 'no-trailing-spaces', 'no-whitespace-before-property', 'object-curly-spacing', 'operator-linebreak', 'quote-props', 'quotes', 'semi', 'semi-spacing', 'semi-style', 'space-before-blocks', 'space-before-function-paren', 'space-in-parens', 'space-infix-ops', 'space-unary-ops', 'spaced-comment', 'switch-colon-spacing', 'template-curly-spacing', 'wrap-iife' ];
-const formattingRules = {};
-formattingRuleNames.forEach( function ( ruleName ) {
-	formattingRules[ ruleName ] = config.rules[ ruleName ];
-} );
-config.rules = formattingRules;
 
 const templates = {};
 [
@@ -38,28 +25,19 @@ function mdLink( target, label ) {
 }
 
 function buildRuleDetails( tests, icon, showFixes ) {
-
-	function lintFixList( codeList ) {
-		// Concatentate into one code block to improve performance
-		const separator = '\n/* - */\n';
-		const codeBlock = codeList.join( ';' + separator );
-		// Add an extra semicolon to avoid syntax error
-		return linter.verifyAndFix( codeBlock, config ).output
-			.split( separator ).map( ( code ) => code.trim() );
-	}
-
+	const batchLintFix = require( './fix' ).batchLintFix;
+	const testsByOptions = {};
 	let output = '';
 	let maxCodeLength;
-	const testsByOptions = {};
 
-	const fixedCode = lintFixList( tests.map( ( test ) => typeof test === 'string' ? test : test.code ) );
+	const fixedCode = batchLintFix( tests.map( ( test ) => typeof test === 'string' ? test : test.code ) );
 
 	let fixedOutput;
 	if ( showFixes ) {
 		maxCodeLength = fixedCode.reduce( function ( acc, code ) {
 			return Math.max( acc, code.length );
 		}, 0 );
-		fixedOutput = lintFixList( tests.map( ( test ) => test.output || 'null' ) );
+		fixedOutput = batchLintFix( tests.map( ( test ) => test.output || 'null' ) );
 	}
 
 	tests.forEach( function ( test, i ) {
