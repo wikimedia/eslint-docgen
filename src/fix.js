db7bb11e9b3e081a86970fff55897b5ff59795a8
Ed Sanders <ejsanders@gmail.com>
2020-05-14 16:58:19 +0100

    Choose lint-and-fix rules based on rule metadata
    Fixes #4

--- src/fix.js
+++ src/fix.js
@@ -2,15 +2,19 @@ const eslint = require( 'eslint' );
 const linter = new eslint.Linter();
 const cli = new eslint.CLIEngine();
 const config = cli.getConfigForFile( require( './main' ) );
+const rules = cli.getRules();
 
-// Restrict config used by verifyAndFix to fixable formatting rules
-// TODO: Do this by looking at rule metadata
-const formattingRuleNames = [ 'array-bracket-spacing', 'block-spacing', 'brace-style', 'comma-dangle', 'comma-spacing', 'comma-style', 'computed-property-spacing', 'curly', 'dot-location', 'dot-notation', 'func-call-spacing', 'indent', 'key-spacing', 'keyword-spacing', 'linebreak-style', 'no-extra-semi', 'no-irregular-whitespace', 'no-mixed-spaces-and-tabs', 'no-multi-spaces', 'no-regex-spaces', 'no-tabs', 'no-trailing-spaces', 'no-whitespace-before-property', 'object-curly-spacing', 'operator-linebreak', 'quote-props', 'quotes', 'semi', 'semi-spacing', 'semi-style', 'space-before-blocks', 'space-before-function-paren', 'space-in-parens', 'space-infix-ops', 'space-unary-ops', 'spaced-comment', 'switch-colon-spacing', 'template-curly-spacing', 'wrap-iife' ];
-const formattingRules = {};
-formattingRuleNames.forEach( function ( ruleName ) {
-	formattingRules[ ruleName ] = config.rules[ ruleName ];
+// Optimization: Only use fixable rules
+const fixableRules = {};
+Object.keys( config.rules ).forEach( function ( ruleName ) {
+	if ( rules.has( ruleName ) ) {
+		const rule = rules.get( ruleName );
+		if ( rule.meta.fixable ) {
+			fixableRules[ ruleName ] = config.rules[ ruleName ];
+		}
+	}
 } );
-config.rules = formattingRules;
+config.rules = fixableRules;
 
 function lintFix( code ) {
 	return linter.verifyAndFix( code, config ).output;
