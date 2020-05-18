efdc3ac8404ccc7d00b8d828f83579554343afbf
Ed Sanders <ejsanders@gmail.com>
2020-05-18 11:37:44 +0100

    Pre-compile templates

--- src/load-templates.js
+++ src/load-templates.js
@@ -1,12 +1,14 @@
 const fs = require( 'fs' );
 const path = require( 'path' );
+const ejs = require( 'ejs' );
 
 function loadTemplates( dirPath ) {
 	const files = fs.readdirSync( dirPath );
 	const templates = {};
 	files.forEach( ( template ) => {
-		templates[ path.parse( template ).name ] =
-			fs.readFileSync( path.join( dirPath, template ) ).toString();
+		templates[ path.parse( template ).name ] = ejs.compile(
+			fs.readFileSync( path.join( dirPath, template ) ).toString()
+		);
 	} );
 	return templates;
 }
