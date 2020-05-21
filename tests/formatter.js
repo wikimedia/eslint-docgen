c020b14721921f33018fbfa30b2d9fc8362f5c47
Ed Sanders <ejsanders@gmail.com>
2020-05-22 00:35:34 +0100

    Use regex for formatter test

--- tests/formatter.js
+++ tests/formatter.js
@@ -3,8 +3,8 @@ const assert = require( 'assert' );
 describe( 'formatter', () => {
 	it( 'formatter', () => {
 		const formatter = require( '../src/formatter' );
-		assert.strictEqual( formatter.warning( 'foo' ), '\u001b[33mwarning\u001b[39m foo' );
-		assert.strictEqual( formatter.error( 'bar' ), '\u001b[31merror\u001b[39m bar' );
-		assert.strictEqual( formatter.heading( 'baz' ), '\u001b[4mbaz\u001b[24m' );
+		assert.ok( formatter.warning( 'foo' ).match( /warning.*foo/ ) );
+		assert.ok( formatter.error( 'bar' ).match( /error.*bar/ ) );
+		assert.ok( formatter.heading( 'baz' ).match( /baz/ ) );
 	} );
 } );
