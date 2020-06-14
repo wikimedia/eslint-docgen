637bbd9d2047a009df24cb0eaedb97f6c876e004
Ed Sanders <ejsanders@gmail.com>
2020-06-14 20:21:48 +0100

    Allow package.main to be empty
    Upstream it has a default value of index.js, use that here.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const packagePath = require( './package-path' );
-const main = require( packagePath( './package' ) ).main;
+const main = require( packagePath( './package' ) ).main || 'index.js';
 
 module.exports = packagePath( main );
