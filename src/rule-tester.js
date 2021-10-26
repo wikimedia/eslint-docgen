11340fa263d654c17b1a8129dd25dfb6f42815b4
Ed Sanders <ejsanders@gmail.com>
2021-10-26 18:06:00 +0100

    Drop deprecated 'noDoc' option and '--doc' CLI flag

--- src/rule-tester.js
+++ src/rule-tester.js
@@ -3,37 +3,26 @@
 const packagePath = require( './package-path' );
 // Use plugin's version of ESLint
 const ESLintRuleTester = require( packagePath( 'node_modules/eslint' ) ).RuleTester;
-const docMode = !!process.env.DOCGEN || process.argv.includes( '--doc' );
-
-if ( process.argv.includes( '--doc' ) ) {
-	const formatter = require( './formatter' );
-	console.log(
-		formatter.warn( '--doc is deprecated, use the DOCGEN=1 environment variable instead.' )
-	);
-}
+const inDocMode = !!process.env.DOCGEN;
 
 /**
  * Extends ESLint's RuleTester to also build documentation
  */
 class RuleTester extends ESLintRuleTester {
 	run( name, rule, tests ) {
-		if ( docMode ) {
+		if ( inDocMode ) {
 			RuleTester.it( name, ( done ) => {
 				const writeDocsFromTests = require( './write-docs-from-tests' );
 				writeDocsFromTests( name, rule, tests, this.testerConfig, done );
 			} );
 		} else {
-			// Filter out invalid property "noDoc"
+			// Filter out invalid property "docgen"
 			// (used in documentation building mode).
 			tests.valid.forEach( ( test ) => {
 				delete test.docgen;
-				// Deprecated
-				delete test.noDoc;
 			} );
 			tests.invalid.forEach( ( test ) => {
 				delete test.docgen;
-				// Deprecated
-				delete test.noDoc;
 			} );
 
 			// Filter out invalid top level property "docgenConfig"
